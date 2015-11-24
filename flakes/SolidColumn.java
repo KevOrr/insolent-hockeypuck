bce37ce001a1d083519d2a9949e0c80101802f33
Kevin Orr <kevinorr54@gmail.com>
2015-11-24 11:07:45 -0500

    Fixed flakes.SolidColumn

--- flakes/SolidColumn.java
+++ flakes/SolidColumn.java
@@ -1,6 +1,6 @@
 /* Program:    SolidColumn.java
  * Student:    Kevin Orr, Seung-Yeon Lee
- * Desc:       
+ * Desc:       #2 Solid Columns
  */
 
 package grouplab2.flakes;
@@ -8,5 +8,5 @@ package grouplab2.flakes;
 import grouplab2.MeltableSnowFlake;
 
 public class SolidColumn extends MeltableSnowFlake {
-
+    int TYPE=2;
 }
