f33fce911cf0899c7d6f574ed8721fc431928933
Kevin Orr <kevinorr54@gmail.com>
2015-11-24 11:10:47 -0500

    SnowBallFactory now returns the created snowball

--- SnowBallFactory.java
+++ SnowBallFactory.java
@@ -14,13 +14,15 @@ public class SnowBallFactory {
     static Random gen = new Random(System.currentTimeMillis());
 
     public static void main(String[] args) {
-        createSnowBall(25, 0, new ArrayList<MeltableSnowFlake>());
+        ArrayList<MeltableSnowFlake> snowBall = createSnowBall(25, 0, new ArrayList<MeltableSnowFlake>());
+        System.out.println(snowBall);
     }
 
-    private static void createSnowBall(int desiredSize, int currentSize, ArrayList<MeltableSnowFlake> snowBall) {
+    private static ArrayList<MeltableSnowFlake> createSnowBall(int desiredSize, int currentSize, ArrayList<MeltableSnowFlake> snowBall) {
         if (currentSize < desiredSize) {
             snowBall.add(SnowFlake.makeRandomSnowflake());
             createSnowBall(currentSize + 1, desiredSize, snowBall);
         }
+        return snowBall;
     }
 }
