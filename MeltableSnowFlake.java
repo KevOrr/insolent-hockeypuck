a9f9cf564b99981cbc8775c1cd0ced7477561949
Kevin Orr <kevinorr54@gmail.com>
2015-12-01 17:18:23 -0500

    Cleaned up

--- MeltableSnowFlake.java
+++ MeltableSnowFlake.java
@@ -16,36 +16,36 @@ public class MeltableSnowFlake extends SnowFlake {
     }
 
     public static MeltableSnowFlake makeRandomSnowflake() {
+        MeltableSnowFlake flake;
         switch (gen.nextInt(35) + 1) {
-            case 1:  return new SimplePrism();
-            case 2:  return new SolidColumn();
-            case 3:  return new Sheath();
-            case 4:  return new ScrollsOnPlate();
-            case 5:  return new TriangularForm();
-            case 6:  return new HexagonalPlate();
-            case 7:  return new HollowColumn();
-            case 8:  return new Cup();
-            case 9:  return new ColumnsOnPlate();
-            case 10: return new TwelveBranchedStar();
-            case 11: return new StellarPlate();
-            case 12: return new BulletRosette();
-            case 13: return new CappedColumn();
-            case 14: return new SplitPlatesAndStars();
-            case 15: return new RadiatingPlate();
-            case 16: return new SectoredPlate();
-            case 17: return new IsolatedBullet();
-            case 18: return new MultiplyCappedColumn();
-            case 19: return new SkeletalForm();
-            case 20: return new RadiatingDendrite();
-            case 21: return new SimpleStar();
-            case 22: return new SimpleNeedle();
-            case 23: return new CappedBullets();
-            case 24: return new TwinColumn();
-            case 25: return new Irregular();
-            
-            // case ...
-            default: return new MeltableSnowFlake(); //Unreachable, but won't compile without
+            case 1:  flake = new SimplePrism();
+            case 2:  flake = new SolidColumn();
+            case 3:  flake = new Sheath();
+            case 4:  flake = new ScrollsOnPlate();
+            case 5:  flake = new TriangularForm();
+            case 6:  flake = new HexagonalPlate();
+            case 7:  flake = new HollowColumn();
+            case 8:  flake = new Cup();
+            case 9:  flake = new ColumnsOnPlate();
+            case 10: flake = new TwelveBranchedStar();
+            case 11: flake = new StellarPlate();
+            case 12: flake = new BulletRosette();
+            case 13: flake = new CappedColumn();
+            case 14: flake = new SplitPlatesAndStars();
+            case 15: flake = new RadiatingPlate();
+            case 16: flake = new SectoredPlate();
+            case 17: flake = new IsolatedBullet();
+            case 18: flake = new MultiplyCappedColumn();
+            case 19: flake = new SkeletalForm();
+            case 20: flake = new RadiatingDendrite();
+            case 21: flake = new SimpleStar();
+            case 22: flake = new SimpleNeedle();
+            case 23: flake = new CappedBullets();
+            case 24: flake = new TwinColumn();
+            case 25: flake = new Irregular();
+            default: flake = new MeltableSnowFlake(); //Unreachable, but won't compile without
         }
+        return flake;
     }
 
 }
